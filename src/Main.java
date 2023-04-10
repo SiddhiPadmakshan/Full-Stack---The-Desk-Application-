f6a4372f098f8cee39b5f7b6ec7fb7d673a0aeb8
Siddhi Padmakshan <siddhipadmakshan21@gmail.com>
2023-04-10 12:24:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -21,10 +22,7 @@ public class Main {
         };
         int[] arr1 = {1,2,3,4,5,6};
         int  slen = arr1.length;
-        for(int i=0; i<slen;i++){
-            System.out.println(arr[i]);
-            // display the all the Strings mentioned in the String array
-        }
+        
         ArrayList<Integer> arrlist = new ArrayList<Integer>();
         ArrayList<Integer> expenses = new ArrayList<Integer>();
         expenses.add(1000);
@@ -35,14 +33,19 @@ public class Main {
         expenses.addAll(arrlist);
         System.out.println("\nEnter your choice:\t");
         Scanner sc = new Scanner(System.in);
-        int  options =  sc.nextInt();
-        for(int j=1;j<=slen;j++){
-            if(options==j){
+       
+        boolean exit = false;
+
+        while (!exit){
+            for(int i=0; i<slen;i++){
+                System.out.println(arr[i]);
+                // display the all the Strings mentioned in the String array
+            }
+            int  options =  sc.nextInt();
                 switch (options){
                     case 1:
                         System.out.println("Your saved expenses are listed below: \n");
-                        System.out.println(expenses+"\n");
-                        optionsSelection();
+                        System.out.println(expenses+"\n"); 
                         break;
                     case 2:
                         System.out.println("Enter the value to add your Expense: \n");
@@ -51,8 +54,6 @@ public class Main {
                         System.out.println("Your value is updated\n");
                         expenses.addAll(arrlist);
                         System.out.println(expenses+"\n");
-                        optionsSelection();
-
                         break;
                     case 3:
                         System.out.println("You are about the delete all your expenses! \nConfirm again by selecting the same option...\n");
@@ -64,37 +65,105 @@ public class Main {
                         } else {
                             System.out.println("Oops... try again!");
                         }
-                        optionsSelection();
                         break;
                     case 4:
-                        sortExpenses(expenses);
-                        optionsSelection();
+                        int [] sortedExpenses = sortExpenses(expenses);
+                        expenses.clear();
+                        for(int i = 0; i < sortedExpenses.length; i++){
+                            expenses.add(sortedExpenses[i]);
+                        }
+                        expenses.addAll(arrlist);
+                        System.out.println("\n"+"Your saved expenses are sorted below: \n");
+                        System.out.println(expenses+"\n");
                         break;
                     case 5:
                         searchExpenses(expenses);
-                        optionsSelection();
                         break;
                     case 6:
                         closeApp();
+                        exit = true;
                         break;
                     default:
-                        System.out.println("You have made an invalid choice!");
+                        System.out.println("You have made an invalid choice!\n");
                         break;
                 }
-            }
+            
         }
+        
 
     }
     private static void closeApp() {
         System.out.println("Closing your application... \nThank you!");
             }
+    
     private static void searchExpenses(ArrayList<Integer> arrayList) {
         int leng = arrayList.size();
         System.out.println("Enter the expense you need to search:\t");
-        //Complete the method
+        Scanner sc = new Scanner(System.in);
+        int response = sc.nextInt();
+        for(int i = 0; i < leng; i++){
+            if(response == arrayList.get(i)){
+                String value = "";
+                if(i == 0){
+                    value = "st";
+                }
+                else if(i == 1){
+                    value = "nd";
+                }
+                else if(i == 2){
+                    value = "rd";
+                }
+                else{
+                    value = "th";
+                }
+                System.out.println("\nExpenditure of " + arrayList.get(i) + " is your " + (i+1) + value + " Expenditure in the expense list.\n");
+                break;
+            }
+            if(i == (arrayList.size() - 1) && response != arrayList.get(i)){
+                System.out.println("\nNo such expenditure is seen. Please try again with a different value.\n");
+                System.out.println(arrayList.get(i)); 
+            }   
+        }
+        
     }
-    private static void sortExpenses(ArrayList<Integer> arrayList) {
+   
+    private static int[] sortExpenses(ArrayList<Integer> arrayList) {
         int arrlength =  arrayList.size();
        //Complete the method. The expenses should be sorted in ascending order.
+       int[] arr = new int[arrayList.size()];
+
+       for (int i = 0; i < arrlength; i++){
+        arr[i] = arrayList.get(i);
+       }
+        quickSort(arr, 0, arrlength - 1);
+        return arr;
+        
     }
+    
+    public static void quickSort(int[] arr, int left, int right) {
+		if ( left < right) {
+			int pivotIndex = partition(arr,left,right);
+			quickSort(arr, left, pivotIndex-1);
+			quickSort(arr, pivotIndex+1, right);
+		}
+	}
+    private static int partition(int[] arr, int left, int right) {
+		int pivot = arr[left];
+		int storeIndex = left + 1;
+		
+		for ( int i = left + 1; i <= right ;i++) {
+			if ( arr[i] < pivot || arr[i] == pivot && Math.random() < 0.5) {
+				swap(arr,i,storeIndex);
+				storeIndex++;
+			}
+		}
+		swap ( arr, left, storeIndex -1 );
+		return storeIndex -1;
+		
+	}
+    private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 }
